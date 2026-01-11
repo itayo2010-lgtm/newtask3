08d29dd97761a7836decf28369252a1fbc32c884
Owner <Owner@CND35217K3>
2026-01-11 20:54:47 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Task3()
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+
+            string output = "";
+            int newnum;
+
+
+            do
+            {
+                Console.WriteLine("enter a new number");
+                newnum = int.Parse(Console.ReadLine());
+                if (newnum == 0)
+                    break;
+                if (num % 2 == 0)
+                {
+                    output += newnum + " " + newnum + " ";
+                }
+                else
+                {
+                    output += newnum + " ";
+                }
+            } while (newnum != 0);
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            Task3();
+        }
+    }
+}
+
