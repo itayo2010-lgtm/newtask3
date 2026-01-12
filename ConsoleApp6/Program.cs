509d9d49f4c4a3e3c047d876fd0972d30492ea07
itayo2010-lgtm <itayo2010@ofekschool.org.il>
2026-01-12 09:52:42 +0200

    Update Program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -8,6 +8,33 @@ namespace ConsoleApp6
 {
     internal class Program
     {
+                static void Task2()
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+
+            int sumeven = 0;
+            int sumodd = 0;
+
+            int max = num % 10;
+            
+
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                if (digit > max)
+                    max = digit;
+                if (digit % 2  == 0)
+                    sumeven += digit;
+                else
+                    sumodd += digit;
+                num /= 10;
+            }
+            Console.WriteLine("the sum of even numbers is " + sumeven + " the sum of odd numbers is " + sumodd + " and the biggest number is " + max);
+        }
+
+        
         static void Task3()
         {
             Console.WriteLine("enter a number");
